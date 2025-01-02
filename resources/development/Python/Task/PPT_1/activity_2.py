5e444346eb49495f2bc576caf173a2e24bb221f8
hustavojhon <hustavojhon@gmail.com>
2025-01-02 15:00:00 -0500

    remove: pyhon tasks

--- resources/development/Python/Task/PPT_1/activity_2.py
+++
@@ -1,1 +0,0 @@
-# Una empresa constructora vende terrenos de la siguiente forma (ver figura), elaborar un programa para hallar el area del terreno si solo se ingresan dos longitudes (A, B)
--- resources/development/Python/Task/PPT_3/gt_1.py
+++
@@ -1,4 +0,0 @@
-# Ingresa: el sueldo base de un vendedor y el importe de sus ventas que realizo durante un determinado mes. Calcula su comision de ventas, que es el 8% de importe de sus ventas. Finalmente calcula su sueldo neto (suma sueldo base y su comision de venta)
-
-print("\n========== INGRESO DE DATOS ==========\n")
-po
